05f3fe74b189604a8a79a03f20e8648ce0b25381
Daniel Keith <danielrkeith@mac.lan>
2025-10-25 17:42:06 +1100

    Week 12

---
+++ week12/.Menti.java
@@ -0,0 +1,11 @@
+public class Menti {
+
+    private boolean simplerReturn(int i) {
+        if (i > 3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
